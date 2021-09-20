6008261877f06ac5c9dc5596c86c015631d38a2d
Shi-Ning Sun <ssun3@caltech.edu>
2021-09-19 22:43:58 -0700

    Use qiskit_op instead of openfermion_op for hamiltonian_arr

--- src/greens_function.py
+++ src/greens_function.py
@@ -59,13 +59,14 @@ class GreensFunction:
 
         # Extract the Hamiltonian operators and apply shift and scaling factors
         self.qiskit_op = self.hamiltonian.qiskit_op.copy()
-        self.openfermion_op = copy.deepcopy(self.hamiltonian.openfermion_op)
+        # self.openfermion_op = copy.deepcopy(self.hamiltonian.openfermion_op)
         self.qiskit_op *= scaling
         self.qiskit_op.primitive.coeffs[0] += shift / HARTREE_TO_EV
-        self.openfermion_op *= scaling
-        self.openfermion_op.terms[()] += shift / HARTREE_TO_EV
-        self.hamiltonian_arr = get_sparse_operator(
-            self.openfermion_op).toarray()
+        # self.openfermion_op *= scaling
+        # self.openfermion_op.terms[()] += shift / HARTREE_TO_EV
+        # self.hamiltonian_arr = get_sparse_operator(
+        #    self.openfermion_op).toarray()
+        self.hamiltonian_arr = self.qiskit_op.to_matrix()
         
         self.optimizer = L_BFGS_B() if optimizer is None else optimizer
         if q_instance is None:
@@ -106,7 +107,7 @@ class GreensFunction:
         # are to be calculated
         self.states = states
         self.states_arr = np.empty((self.n_orb, self.n_orb), dtype=np.object)
-        print('states_arr\n', self.states_arr)
+        # print('states_arr\n', self.states_arr)
         # self.states_arr = np.array([[[None] for i in range(self.n_orb)] for j in range(self.n_orb)])
 
         # Variables to store eigenenergies and eigenstates
@@ -149,15 +150,18 @@ class GreensFunction:
     def compute_eh_states(self) -> None:
         """Calculates (N±1)-electron states of the Hamiltonian."""
         print("Start calculating (N±1)-electron states")
+        print("=" * 80)
         self.eigenenergies_e, self.eigenstates_e = \
-            number_state_eigensolver(self.hamiltonian_arr, self.n_occ + 1, reverse=False)
+            number_state_eigensolver(self.hamiltonian_arr, self.n_occ + 1, reverse=True)
+        print("=" * 80)
         self.eigenenergies_h, self.eigenstates_h = \
-            number_state_eigensolver(self.hamiltonian_arr, self.n_occ - 1, reverse=False)
+            number_state_eigensolver(self.hamiltonian_arr, self.n_occ - 1, reverse=True)
         self.eigenenergies_e *= HARTREE_TO_EV
         self.eigenenergies_h *= HARTREE_TO_EV
-        print(self.eigenenergies_e)
-        print(self.eigenenergies_h)
+        print("eigenstates_e\n", self.eigenstates_e)
+        print("eigenstates_h\n", self.eigenstates_h)
 
+        '''
         eigenenergies_2, eigenstates_2 = number_state_eigensolver(self.hamiltonian_arr, 2)
         if True:
             np.set_printoptions(precision=2)
@@ -177,6 +181,7 @@ class GreensFunction:
             res[abs(res) < 1e-8] = 0
             print('IZIZ values')
             print(res)
+        
 
         # print(self.eigenstates_e.shape)
         if True:
@@ -192,6 +197,7 @@ class GreensFunction:
             print('IZIZ values\n')
             print(eigenstates.conj().T @ IZIZ @ eigenstates)
         print("Finish calculating (N±1)-electron states")
+    '''
 
     def compute_diagonal_amplitudes(self,
                                     cache_read: bool = True,
@@ -212,14 +218,24 @@ class GreensFunction:
                     psi = result.get_statevector()
                     
                     if self.states in ('e', None):
-                        inds_e = get_number_state_indices(self.n_orb, self.n_occ + 1, anc='1', reverse=True)
+                        inds_e = get_number_state_indices(
+                            self.n_orb, self.n_occ + 1, anc='1', reverse=True)
+                        print('-' * 80)
+                        print('m =', m)
+                        print(psi[inds_e])
+                        print('-' * 80)
                         probs_e = np.abs(self.eigenstates_e.conj().T @ psi[inds_e]) ** 2
                         probs_e[abs(probs_e) < 1e-8] = 0.
                         # print('probs_e =', probs_e)
                         self.B_e[m, m] = probs_e
 
                     if self.states in ('h', None):                     
-                        inds_h = get_number_state_indices(self.n_orb, self.n_occ - 1, anc='0', reverse=True)
+                        inds_h = get_number_state_indices(
+                            self.n_orb, self.n_occ - 1, anc='0', reverse=True)
+                        print('-' * 80)
+                        print('m =', m)
+                        print(psi[inds_h])
+                        print('-' * 80)
                         probs_h = np.abs(self.eigenstates_h.conj().T @ psi[inds_h]) ** 2
                         probs_h[abs(probs_h) < 1e-8] = 0.
                         # print('probs_h =', probs_h)
@@ -441,7 +457,7 @@ class GreensFunction:
                         np.exp(-1j * np.pi / 4) * (self.D_ep[m, n] - self.D_em[m, n]) +
                         np.exp(1j * np.pi / 4) * (self.D_ep[n, m] - self.D_em[n, m]))
                     self.B_h[m, n] = (
-                        np.exp(-1j * np.pi / 4) * (self.D_hp[m, n] - self.D_hm[m, n]) + \
+                        np.exp(-1j * np.pi / 4) * (self.D_hp[m, n] - self.D_hm[m, n]) +
                         np.exp(1j * np.pi / 4) * (self.D_hp[n, m] - self.D_hm[n, m]))
 
         print('B_e\n')
