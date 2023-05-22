907b3d9bea2688bfdc4ce2943872442bb4fdd00b
Shi-Ning Sun <snsunx@gmail.com>
2023-05-22 16:33:54 -0700

    Modify Fig 3 plotting script

--- plots/fig3_spectral_function/plot_fig3_spectral_function.py
+++ plots/fig3_spectral_function/plot_fig3_spectral_function.py
@@ -15,82 +15,74 @@ plt.rcParams.update({
     'figure.subplot.bottom': 0.17,
     'figure.subplot.wspace': 0.0,
     'lines.linewidth': 2,
-    'lines.markersize': 8
+    'lines.markersize': 8,
+    'lines.markeredgewidth': 2.5
 })
 
+np.set_printoptions(formatter={'float': '{: 6.3f}'.format})
 
-def plot_A(
+
+def plot_spectral_function(
+    fig: plt.Figure,
     ax: plt.Axes,
-    # mol_name: str,
     datfname_exact: str,
-    datfname_expt: str, 
+    datfname_expt: str,
     panel_name: str,
-    # include_xlabel: bool = True,
-    include_ylabel: bool = True,
-    include_legend: bool = True,
 ) -> None:
-    """Plots the spectral function in Fig. 2.
+    """Plots the spectral function in Fig. 3.
     
     Args:
+        fig: The Figure object.
         ax: The Axes object.
-        mol_name: The molecule name. "nah" or "kh".
-        panel_name:  The text to be put in the panel label, e.g. "(a)".
-        include_xlabel: Whether to include x-axis label.
-        include_ylabel: Whether to include y-axis label.
+        datfname_exact: The exact data file name.
+        datfname_expt: The experimental data file name.
+        panel_name:  The text to be put in the panel label, e.g. "A".
     """
-    global fig
-
+    # Load and plot the exact data
     omegas, As = np.loadtxt(datfname_exact + ".dat").T
     ax.plot(omegas, As, color='k', label="Exact")
     peaks, _ = find_peaks(As)
     amps_exact = As[peaks]
 
+    # Load and plot the experimental data
     omegas, As = np.loadtxt(datfname_expt + ".dat").T
     ax.plot(omegas, As, ls='--', lw=3, color='xkcd:medium blue', marker='x', markevery=0.12, label="Expt.")
     peaks, _ = find_peaks(As)
     amps_expt = As[peaks]
 
-    print("amps_exact", amps_exact)
-    print("amps_expt", amps_expt)
-    print("percentage difference", (amps_expt - amps_exact) / amps_exact)
-
+    # Set axis labels and legends
     ax.text(0.92, 0.9, r"\textbf{" + panel_name + "}", transform=ax.transAxes)
-
     ax.set_xlabel("$\omega$ (eV)")
-    if include_ylabel:
+    if panel_name == 'A':
         ax.set_ylabel("$A$ (eV$^{-1}$)")
-    ax.set_yticks([0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
-    if include_legend:
         ax.legend(loc='center', bbox_to_anchor=(0.23, 0.87, 0.0, 0.0), frameon=False)
+    ax.set_yticks([0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
+
+    # Print out information about amplitudes
+    print("Exact peaks (eV^-1):       ", amps_exact)
+    print("Expt. peaks (eV^-1):       ", amps_expt)
+    print("Percentage difference (%): ", (amps_expt - amps_exact) / amps_exact)
 
 
 def main():
-    print("Start plotting data.")
-    global fig
+    print("> Start plotting data.")
 
+    # Data file names
     dirname = "sep13_2022"
+    datfname_nah_exact = f"../../expt/greens/{dirname}/data/obs/nah_greens_exact_A"
+    datfname_nah_expt  = f"../../expt/greens/{dirname}/data/obs/nah_greens_tomo_expt_A"
+    datfname_kh_exact  = f"../../expt/greens/{dirname}/data/obs/kh_greens_exact_A"
+    datfname_kh_expt   = f"../../expt/greens/{dirname}/data/obs/kh_greens_tomo_expt_A"
 
+    # Create the figure and axis objects
     fig, axes = plt.subplots(1, 2, figsize=(10, 5), sharey=True)
 
-    plot_A(
-        axes[0],
-        f'../../expt/greens/{dirname}/data/obs/nah_greens_exact_A',
-        f'../../expt/greens/{dirname}/data/obs/nah_greens_tomo_expt_A',
-        'A',
-        include_legend=True
-    )
-    plot_A(
-        axes[1],
-        f'../../expt/greens/{dirname}/data/obs/kh_greens_exact_A',
-        f'../../expt/greens/{dirname}/data/obs/kh_greens_tomo_expt_A',
-        'B',
-        include_ylabel=False,
-        include_legend=False
-    )
-
+    # Plot the figures
+    plot_spectral_function(fig, axes[0], datfname_nah_exact, datfname_nah_expt, panel_name='A')
+    plot_spectral_function(fig, axes[1], datfname_kh_exact, datfname_kh_expt, panel_name='B')
     fig.savefig("fig3_spectral_function.png", dpi=300)
 
-    print("Finished plotting data.")
+    print("> Finished plotting data.")
 
 
 if __name__ == '__main__':
