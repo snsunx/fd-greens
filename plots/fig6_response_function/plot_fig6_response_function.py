f8112a482c29c06e8040f7bb29e00e5768c49dd7
Shi-Ning Sun <snsunx@gmail.com>
2023-05-22 19:42:06 -0700

    Clean up plotting script for Figure 6

--- plots/fig6_response_function/plot_fig6_response_function.py
+++ plots/fig6_response_function/plot_fig6_response_function.py
@@ -5,12 +5,12 @@ from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 
 plt.rcParams.update({
-	'text.usetex': True,
-	'text.latex.preamble': r'\usepackage{amsmath}',
+    'text.usetex': True,
+    'text.latex.preamble': r'\usepackage{amsmath}',
     'font.family': 'sans-serif',
     'font.sans-serif': 'Helvetica',
     'font.size': 23,
-    'figure.subplot.left': 0.12,
+    'figure.subplot.left': 0.1,
     'figure.subplot.right': 0.98,
     'figure.subplot.top': 0.94,
     'figure.subplot.bottom': 0.07,
@@ -21,118 +21,97 @@ plt.rcParams.update({
     'lines.markeredgewidth': 3,
 })
 
-def rms_error(predictions, targets):
-    """RMS error between experimental and exact data."""
-    return np.sqrt(((predictions - targets) ** 2).mean()) * 1000
 
-def plot_chi_itoffoli_vs_cz(
+def plot_response_function(
     fig: plt.Figure,
     ax: plt.Axes,
     datfname_exact: str,
     datfname_itof: str,
     datfname_cz: str,
-    use_real_part: bool = False,
+    mol_name: str,
     panel_name: str = 'B',
-    include_legend: bool = False,
-    include_xlabel: bool = True,
-    include_ylabel: bool = True,
 ) -> None:
     """Plots the response function by comparing iToffoli vs CZ decompositions."""
     # Print header
     if panel_name == 'A':
-        print(f"========== Panel A: chi00 without RC ==========")
+        print("=============== Panel A: chi00 without RC ===============")
     elif panel_name == 'B':
-        print("========== Panel B: chi00 with RC ===========")
+        print("=============== Panel B: chi00 with RC    ===============")
     elif panel_name == 'C':
-        print("========== Panel C: chi01 without RC ===========")
+        print("=============== Panel C: chi01 without RC ===============")
     elif panel_name == 'D':
-        print("========== Panel D: chi01 with RC ==========")
+        print("=============== Panel D: chi01 with RC    ===============")
     
-    # Load response function from data file
-    omegas, reals, imags = np.loadtxt(datfname_exact + ".dat").T
-    obs_exact = reals if use_real_part else imags
-    peaks, _ = find_peaks(obs_exact, height=0.01)
-    amps_exact = obs_exact[peaks]
+    # Plot the exact data
+    omegas, _, obs_exact = np.loadtxt(datfname_exact + ".dat").T
     ax.plot(omegas, obs_exact, color='k', label="Exact")
 
+    # Find peak heights from exact data
+    peaks_exact, _ = find_peaks(obs_exact, height=0.01)
+    if panel_name == 'C':
+        peaks_exact = peaks_exact[::-1]
+    amps_exact = obs_exact[peaks_exact]
+
     # Plot the iToffoli data
-    omegas, reals, imags = np.loadtxt(datfname_itof + ".dat").T
-    obs = reals if use_real_part else imags
+    omegas, _, obs_itof = np.loadtxt(datfname_itof + ".dat").T
     ax.plot(
-        omegas, obs, ls='--', marker='+', 
-        ms=plt.rcParams['lines.markersize'] + 2, 
+        omegas, obs_itof, ls='--', marker='+',
+        ms=plt.rcParams['lines.markersize'] + 2,
         markevery=0.12, color='xkcd:medium blue',
         label="iToffoli")
-
-    # Calculate and print out the RMS error
-    # print(f'rms_error (itof) = {rms_error(obs_exact, obs):.1f} meV')
-
-    # Calculate and print out the peak height deviation
-    peaks_itof, _ = find_peaks(obs, height=0.01)
-    amps_itof = obs[peaks_itof]
+    
+    # Find peak heights from iToffoli data
+    peaks_itof, _ = find_peaks(obs_itof, height=0.01)
+    amps_itof = obs_itof[peaks_itof]
     deviation_itof = (amps_itof - amps_exact) / amps_exact * 100
-    print(f'peak height deviation (itof) = {np.array_str(deviation_itof, precision=2)} %')
-
 
     # Plot the CZ data
-    omegas, reals, imags = np.loadtxt(datfname_cz + ".dat").T
-    obs = reals if use_real_part else imags
-    ax.plot(omegas, obs, ls='--', marker='x', markevery=0.12, color='xkcd:pinkish', label="CZ")
-    
-    # Calculate and print out the RMS error
-    # print(f'rms_error (cz) = {rms_error(obs_exact, obs):.1f} meV')
+    omegas, _, obs_cz = np.loadtxt(datfname_cz + ".dat").T
+    ax.plot(omegas, obs_cz, ls='--', marker='x', markevery=0.12, color='xkcd:pinkish', label="CZ")
 
-    # Calculate and print out the peak height deviation
-    peaks_cz, _ = find_peaks(obs, height=0.01)
-    amps_cz = obs[peaks_cz]
+    # Find peak heights from CZ data
+    peaks_cz, _ = find_peaks(obs_cz, height=0.01)
+    amps_cz = obs_cz[peaks_cz]
     deviation_cz = (amps_cz - amps_exact) / amps_exact * 100
-    print(amps_cz, amps_exact)
-    print(f'peak height deviation (cz) = {np.array_str(deviation_cz, precision=2)} %')
 
+    # Print out information about peak heights
+    print(f'Peak locations and amplitudes in exact:    {peaks_exact} {np.array_str(amps_exact, precision=4)}')
+    print(f'Peak locations and amplitudes in iToffoli: {peaks_itof} {np.array_str(amps_itof, precision=4)}')
+    print(f"Peak locations and amplitudes in CZ:       {peaks_cz} {np.array_str(amps_cz, precision=4)}")
+    print(f'Peak height deviation in iToffoli (%):     {np.array_str(deviation_itof, precision=2)}')
+    print(f'Peak height deviation in CZ (%):           {np.array_str(deviation_cz, precision=2)}')
 
-
-    # try:
-    #     print("peaks_itof      ", peaks_itof)
-    #     print("peaks_cz        ", peaks_cz)
-    #     print("amps_itof      ", amps_itof)
-    #     print("amps_cz        ", amps_cz)
-    #     percentage_itof = (amps_itof - amps_exact) / amps_exact * 100
-    #     percentage_cz = (amps_cz - amps_exact) / amps_exact * 100
-    #     print("deviation itof", np.array_str(percentage_itof, precision=2), "%")
-    #     print("deviation cz  ", np.array_str(percentage_cz, precision=2), "%")
-    # except:
-    #     print("Failed to calculate RMS error")
-
+    # Set panel name and axis labels
     ax.text(0.92, 0.9, r"\textbf{" + panel_name + "}", transform=ax.transAxes)
+    ax.set_xlabel("$\omega$ (eV)")
+    ax.set_xticks([-30, -20, -10, 0, 10, 20, 30])
+    ylabel_string = "Im $\chi_{" + datfname_exact[-2:] + "}$ (eV$^{-1}$)"
+    ax.set_ylabel(ylabel_string)
+    if mol_name == 'nah':
+        if panel_name == 'A':
+            ax.set_yticks(np.arange(-0.3, 0.3 + 1e-8, 0.1))
+        elif panel_name == 'C':
+            ax.set_yticks(np.arange(-0.2, 0.2 + 1e-8, 0.1))
+        elif panel_name in ['B', 'D']:
+            ax.set_yticks(np.arange(-0.15, 0.15 + 1e-8, 0.05))
+    elif mol_name == 'kh':
+        ax.set_yticks(np.arange(-0.3, 0.3 + 1e-8, 0.1))
+        
 
-    if include_xlabel:
-        ax.set_xlabel("$\omega$ (eV)")
-        ax.set_xticks([-30, -20, -10, 0, 10, 20, 30])
-    if panel_name in ["A", "C"]:
-        ax.set_yticks([-0.2, -0.1, 0.0, 0.1, 0.2])
-    else:
-        ax.set_yticks([-0.1, 0.0, 0.1])
-    
-    if include_ylabel:
-        prefix = "Re" if use_real_part else "Im"
-        ylabel_string = prefix + " $\chi_{" + datfname_exact[-2:] + "}$ (eV$^{-1}$)"
-        ax.set_ylabel(ylabel_string)
-
-    if include_legend:
-        ax.legend(loc='center', bbox_to_anchor=(0.25, 0.25, 0.0, 0.0), frameon=False, fontsize=22)
-    
+    # Set legend
     if panel_name == "A":
+        ax.legend(loc='center', bbox_to_anchor=(0.25, 0.25, 0.0, 0.0), frameon=False, fontsize=22)
         ax.text(0.5, 1.04, "Without RC", ha='center', transform=ax.transAxes)
     elif panel_name == "B":
         ax.text(0.5, 1.04, "With RC", ha='center', transform=ax.transAxes)
 
     # handles1, labels1 = ax.get_legend_handles_labels()
-    
+
 def main():
     print("> Start plotting data.")
 
     # NOTE: mol_name should be set to 'nah' or 'kh'
-    mol_name = 'nah'
+    mol_name = 'kh'
 
     # Figure parameters
     nrows = 2
@@ -160,16 +139,16 @@ def main():
     ax_d = axes[1, 1]
 
     # Plot the figures
-    plot_chi_itoffoli_vs_cz(fig, ax_a, datfname_exact_00, datfname_itof_a, datfname_cz_a, panel_name='A', include_legend=True)
-    plot_chi_itoffoli_vs_cz(fig, ax_b, datfname_exact_00, datfname_itof_b, datfname_cz_b, panel_name='B')
-    plot_chi_itoffoli_vs_cz(fig, ax_c, datfname_exact_01, datfname_itof_c, datfname_cz_c, panel_name='C')
-    plot_chi_itoffoli_vs_cz(fig, ax_d, datfname_exact_01, datfname_itof_d, datfname_cz_d, panel_name='D')
+    plot_response_function(fig, ax_a, datfname_exact_00, datfname_itof_a, datfname_cz_a, mol_name, panel_name='A')
+    plot_response_function(fig, ax_b, datfname_exact_00, datfname_itof_b, datfname_cz_b, mol_name, panel_name='B')
+    plot_response_function(fig, ax_c, datfname_exact_01, datfname_itof_c, datfname_cz_c, mol_name, panel_name='C')
+    plot_response_function(fig, ax_d, datfname_exact_01, datfname_itof_d, datfname_cz_d, mol_name, panel_name='D')
 
     # Save the figure
     if mol_name == 'nah':
-        fig.savefig(f"fig6_response_function.png", dpi=300)
+        fig.savefig("fig6_response_function.png", dpi=300)
     elif mol_name == 'kh':
-        fig.savefig(f"figs3_response_function_kh.png", dpi=300)    
+        fig.savefig("figs3_response_function_kh.png", dpi=300)
 
     print("> Finished plotting data.")
 
