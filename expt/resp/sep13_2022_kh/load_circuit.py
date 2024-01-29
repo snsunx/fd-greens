2fc97c09260c55149426b54b43e0fc0c76dff184
Shi-Ning Sun <snsunx@gmail.com>
2024-01-29 09:58:20 -0800

    Fix h5py file name error

--- expt/resp/sep13_2022_kh/load_circuit.py
+++ expt/resp/sep13_2022_kh/load_circuit.py
@@ -9,10 +9,10 @@ from fd_greens import CircuitStringConverter, print_circuit_statistics
 
 # NOTE: Valid circuit names are circ0u, circ0d, circ1u, circ1d,
 # circ0u0d, circ0u1u, circ0u1d, circ0d1u, circ0d1d, circ1u1d
-circuit_name = "circ0u"
+circuit_name = "circ0u1d"
 
 # Load the Qtrl strings
-h5file = h5py.File("kh_greens_exact.h5", 'r')
+h5file = h5py.File("kh_resp_exact.h5", 'r')
 strings = json.loads(h5file[f"{circuit_name}/transpiled"][()])
 
 # Convert the Qtrl strings to circuit
